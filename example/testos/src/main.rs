ae42ccd3df1aeb2c89688c2e8e38bb26e23e29bf
Godones <1925466036@qq.com>
2024-07-12 22:18:28 +0800

    refactor the code
    remove unsafe dep

--- example/testos/src/main.rs
+++ example/testos/src/main.rs
@@ -8,9 +8,9 @@ extern crate alloc;
 
 use core::panic::PanicInfo;
 
-use fatfs2::init_fatfs2;
-use vf2_driver::sd::SdHost;
-use vf2_driver::serial;
+// use fatfs2::init_fatfs2;
+// use vf2_driver::sd::SdHost;
+// use vf2_driver::serial;
 use visionfive2_sd::Vf2SdDriver;
 
 use crate::boot::{hart_id, sleep};
@@ -24,30 +24,30 @@ mod boot;
 mod config;
 mod console;
 mod fatfs;
-mod fatfs2;
+// mod fatfs2;
 mod sbi;
 
 pub fn main() {
     boot::clear_bss();
     boot::init_heap();
     console::init_uart(UART_BASE);
-    // console::init_logger();
+    console::init_logger();
     println!("boot hart_id: {}", hart_id());
-    // init_print(&PrePrint);
-    // let sd = Vf2SdDriver::new(sleep);
-    // sd.init();
+    init_print(&PrePrint);
+    let sd = Vf2SdDriver::new(sleep);
+    sd.init();
 
-    serial::init_log(log::LevelFilter::Error).unwrap();
-    let sd = SdHost;
-    sd.init().unwrap();
+    // serial::init_log(log::LevelFilter::Error).unwrap();
+    // let sd = SdHost;
+    // sd.init().unwrap();
 
     println!("sd init ok");
     let mut buf = [0; 512];
     sd.read_block(0, &mut buf);
     println!("buf: {:x?}", &buf[..16]);
 
-    init_fatfs2(sd);
-    // init_fatfs(sd);
+    // init_fatfs2(sd);
+    init_fatfs(sd);
     println!("shutdown.....");
     shutdown();
 }
